a1c6035e6981d85cf74e486203dc9ec84a930e0a
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-02-02 11:35:28 -0600

    change the name "collection" into "mongo_collection"

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school (mongo_collection, **kwargs): 
     """Inserts a new document in a MongoDB collection"""
-    result = collection.insert_one(kwargs)
+    result = mongo_collection.insert_one(kwargs)
     return (result.inserted_id)
