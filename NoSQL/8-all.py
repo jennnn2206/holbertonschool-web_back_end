5bfd78d1d10745b15deb9ff48d50edad4f222a3a
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-02-02 11:02:50 -0600

    adding other comment

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Module to list all documents in a MongoDB collection"""
+
 def list_all(mongo_collection):
      """Lists all documents in a MongoDB collection"""
      if mongo_collection is None: 
