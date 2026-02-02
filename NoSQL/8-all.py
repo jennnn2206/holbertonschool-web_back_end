45ebb25cc0b8fbfb2a9bbc42070cdf6f0072e094
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-02-02 11:00:52 -0600

    changing the comment

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,4 @@
-# Lists all documents in a MongoDB collection
+#!/usr/bin/env python3
 def list_all(mongo_collection):
      """Lists all documents in a MongoDB collection"""
      if mongo_collection is None: 
