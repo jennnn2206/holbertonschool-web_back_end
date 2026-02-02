b96e7805b4c5c08e34f7caf03ba50db64d0497de
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-02-02 11:39:15 -0600

    Change topics on a document based on the name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""Module to change topics on a document based on the name"""
+def update_topics(mongo_collection, name, topics):
+    """Change topics on a document based on the name"""
