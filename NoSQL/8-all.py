9813dc64d9590ec9217d170b17f2455581d65c44
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-02-02 10:59:04 -0600

    Lists all documents in a MongoDB collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,6 @@
+# Lists all documents in a MongoDB collection
+def list_all(mongo_collection):
+     """Lists all documents in a MongoDB collection"""
+     if mongo_collection is None: 
+        return []
+    return list (mongo_collection.find())
