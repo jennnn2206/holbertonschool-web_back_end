d0311393a109355f5c65c8b5bf0e0c4f22ffd8c6
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-02-10 11:13:20 -0600

    creates a trigger that resets the attribute valid_email only when the email has been changed.

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,14 @@
 -- creates a trigger that resets the attribute valid_email only when the email has been changed.
-DELIMITER | 
-CREATE TRIGGER email_validation
-BEFORE UPDATE ON USERS
-FOR EACH ROW 
-BEGIN 
+DELIMITER |
+
+CREATE TRIGGER reset_email_validation
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
     IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END
 |
+
 DELIMITER ;
